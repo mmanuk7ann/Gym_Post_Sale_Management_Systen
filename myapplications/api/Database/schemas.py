296ecc315f31890db1993017a7e9ea500871860e
armenghazaryann <armen.ghazaryan004@gmail.com>
2025-05-12 21:21:13 +0400

    fixing last visit type

--- myapplications/api/Database/schemas.py
+++ myapplications/api/Database/schemas.py
@@ -114,7 +114,7 @@ class PackageCustomerSumResponse(BaseModel):
 class RiskCustomerOut(BaseModel):
     name: str
     email: EmailStr
-    last_visit: date
+    last_visit: int
     membership: str
     inactive_days: int
 
